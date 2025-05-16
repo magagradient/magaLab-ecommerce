c96fef406e452d199f88e185b396b16462dfae3a
Magali Guerzoni <magaliguerzoni@gmail.com>
2025-05-16 01:34:43 -0300

    endpoints downloadLinks y favoriteImages

--- src/controllers/favoriteImages/post/create.js
+++ src/controllers/favoriteImages/post/create.js
@@ -1,10 +1,28 @@
-const { FavoriteImages } = require("../../../database/indexModels");
+const { FavoriteImages, Users, ProductImages } = require("../../../database/indexModels");
 const responseHelper = require('../../../utils/responseHelper');
 
 const create = async (req, res) => {
     const { id_user, id_image } = req.body;
 
     try {
+        const user = await Users.findByPk(id_user);
+        if (!user) {
+            return responseHelper.errorResponse(res, "user_not_found", "Usuario no encontrado.", "favorite_images_create", 404);
+        }
+
+        const image = await ProductImages.findByPk(id_image);
+        if (!image) {
+            return responseHelper.errorResponse(res, "image_not_found", "Imagen no encontrada.", "favorite_images_create", 404);
+        }
+
+        const existingFavorite = await FavoriteImages.findOne({
+            where: { id_user, id_image }
+        });
+
+        if (existingFavorite) {
+            return responseHelper.errorResponse(res, "favorite_already_exists", "La imagen ya está en favoritos.", "favorite_images_create", 409);
+        }
+
         const newFavorite = await FavoriteImages.create({ id_user, id_image });
 
         return responseHelper.successResponse(res, newFavorite, "favorite_images_create");
